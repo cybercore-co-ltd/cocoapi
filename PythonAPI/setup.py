c2c3106a136e2d5cbb0a9f6c6de6f6e4be67319f
thuyngch <thuypn9a4@gmail.com>
2021-03-03 16:12:17 +0900

    rename package

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -6,22 +6,22 @@ import numpy as np
 
 ext_modules = [
     Extension(
-        'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
+        'ytvos_pycocotools._mask',
+        sources=['../common/maskApi.c', 'ytvos_pycocotools/_mask.pyx'],
+        include_dirs=[np.get_include(), '../common'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
 setup(
-    name='pycocotools',
-    packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
+    name='ytvos_pycocotools',
+    packages=['ytvos_pycocotools'],
+    package_dir={'ytvos_pycocotools': 'ytvos_pycocotools'},
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
         'matplotlib>=2.1.0'
     ],
     version='2.0',
-    ext_modules= ext_modules
+    ext_modules=ext_modules
 )
